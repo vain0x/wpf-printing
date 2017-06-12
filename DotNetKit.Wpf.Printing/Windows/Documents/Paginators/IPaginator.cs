30d88a1628d6375e5eb7389a972617270de2a033
vain0 <id_vain@yahoo.co.jp>
2017-06-12 19:08:03 +0900

    Remove PaginatorModule.FromFunc

--- DotNetKit.Wpf.Printing/Windows/Documents/Paginators/IPaginator.cs
+++ DotNetKit.Wpf.Printing/Windows/Documents/Paginators/IPaginator.cs
@@ -18,33 +18,4 @@ namespace DotNetKit.Windows.Documents
         /// </summary>
         IEnumerable Paginate(object printable, Size pageSize);
     }
-
-    sealed class AnonymousPaginator<TReport>
-        : IPaginator
-    {
-        readonly Func<TReport, Size, IEnumerable> paginate;
-
-        public IEnumerable Paginate(TReport page, Size pageSize)
-        {
-            return paginate(page, pageSize);
-        }
-
-        IEnumerable IPaginator.Paginate(object printable, Size pageSize)
-        {
-            return Paginate((TReport)printable, pageSize);
-        }
-
-        public AnonymousPaginator(Func<TReport, Size, IEnumerable> paginate)
-        {
-            this.paginate = paginate;
-        }
-    }
-
-    public static class PaginatorModule
-    {
-        public static IPaginator FromFunc<R>(Func<R, Size, IEnumerable> paginate)
-        {
-            return new AnonymousPaginator<R>(paginate);
-        }
-    }
 }
