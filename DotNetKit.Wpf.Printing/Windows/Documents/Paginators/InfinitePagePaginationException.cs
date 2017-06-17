7732592ae05310a38ce07a95c388cfd60425e2ac
vain0 <id_vain@yahoo.co.jp>
2017-06-17 21:15:25 +0900

    Add InfinitePagePaginationException

---
+++ DotNetKit.Wpf.Printing/Windows/Documents/Paginators/InfinitePagePaginationException.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DotNetKit.Windows.Documents
+{
+    public class InfinitePagePaginationException
+        : InvalidOperationException
+    {
+        public override string Message =>
+            "Page size is too small to display any item in the data grid.";
+    }
+}
