31cccf9f67320cbd34adc219fd9d4fe70f0e6041
vain0 <id_vain@yahoo.co.jp>
2017-06-17 17:58:42 +0900

    Uninstall reactive libraries

--- DotNetKit.Wpf.Printing.Demo/Samples/AsynchronousSample/PrintProgram.cs
+++ DotNetKit.Wpf.Printing.Demo/Samples/AsynchronousSample/PrintProgram.cs
@@ -2,10 +2,6 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Printing;
-using System.Reactive;
-using System.Reactive.Disposables;
-using System.Reactive.Linq;
-using System.Reactive.Subjects;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
